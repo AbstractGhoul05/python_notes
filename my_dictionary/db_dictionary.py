c4533791186b9afc55cf5f1386112c36de3ba4bc
AbstractGhoul05 <aayushb14@gmail.com>
2020-04-26 23:36:51 +0530

    make changes

--- my_dictionary/db_dictionary.py
+++ my_dictionary/db_dictionary.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from difflib import SequenceMatcher, get_close_matches
 
 con = mysql.connector.connect(
     user = "ardit700_student",
@@ -7,9 +8,44 @@ con = mysql.connector.connect(
     database = "ardit700_pm1database"
 )
 
-cursor = con.cursor()
+data_list = list()
 
-query = cursor.execute("SELECT * FROM Dictionary WHERE Expression = 'inlay' ")
-results = cursor.fetchall()
+# cursor = con.cursor()
 
-print(results[0])
+# query = cursor.execute("SELECT * FROM Dictionary WHERE Expression = 'inlay' ")
+# results = cursor.fetchall()
+
+# print(results[0])
+
+def give_meaning(word):
+    cursor = con.cursor()
+    query = cursor.execute(f"SELECT * FROM Dictionary WHERE Expression = '{word}'")
+    meaning_list = cursor.fetchall()
+    if len(meaning_list) == 0:
+        data_list = list()
+        if len(data_list) ==  0:
+            query = cursor.execute("SELECT Expression FROM Dictionary")
+            all_data = cursor.fetchall()
+            data_list = [data[0] for data in all_data]
+        close_words = get_close_matches(word, data_list)
+        for close_word in close_words:
+            control_str = str(input(f"Did you mean {close_word}? (y/n): "))
+            if control_str.lower() == 'y':
+                return give_meaning(close_word)
+        return "No words found!"
+    else:
+        final_meaning = ''
+
+        for meaning in meaning_list:
+            final_meaning += meaning[1] + '\n'
+
+        return final_meaning
+
+
+print("Welcome to my Dictionary!!!\n")
+
+while True:
+    word = str(input("Enter a word: "))
+    if word == "q":
+        break
+    print(give_meaning(word))
--- test.py
+++ test.py
@@ -1,13 +1,15 @@
-from random import shuffle
-from random import randint
+import mysql.connector
+from difflib import SequenceMatcher, get_close_matches
 
-mylist = [1, 2, 3, 4, 5, 6]
-shuffle(mylist)
-print(mylist)
-print(randint(0, 10000))
-inputnum = input('Enter a Number: ')
-print(f'You entered {inputnum}')
-inputint = int(inputnum)
-print(f'You entered {inputint}')
-mylist = [x for x in range(0, 11) if x%2==0]
-print(mylist)
+con = mysql.connector.connect(
+    user = "ardit700_student",
+    password = "ardit700_student",
+    host = "108.167.140.122",
+    database = "ardit700_pm1database"
+)
+
+cursor = con.cursor()
+query = cursor.execute('SELECT Expression FROM Dictionary')
+results = cursor.fetchall()
+print(results[0][0])
+# print(results)
